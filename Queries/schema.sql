7740bce4295cbb473d6d9b049b1e2d1f38b91cb7
Alice Lu <alice@Alice-Laptop.micegroups.com>
2021-04-30 20:24:31 -0700

    add queries file

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE dept_manager (
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -41,8 +41,7 @@ CREATE TABLE dept_emp (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles(
@@ -50,6 +49,6 @@ CREATE TABLE titles(
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATe NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
