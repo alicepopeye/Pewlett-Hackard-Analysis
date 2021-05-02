e7d40aa843a80fb43e7b0624b615c13058633dab
Alice Lu <alice@Alice-Laptop.micegroups.com>
2021-05-02 16:14:05 -0700

    update qury file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -50,3 +50,80 @@ FROM employees as e
 WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 AND de.to_date = '9999-01-01'
 ORDER BY e.emp_no, de.to_date DESC;
+
+--Mentorship group by title
+SELECT 
+	COUNT(emp_no),
+	title
+--INTO mentorship_eligible_title
+FROM mentorship_eligibilty
+GROUP BY title
+ORDER BY count(emp_no) DESC;
+
+--Adddional: How many current/active employees by title?
+SELECT 
+	count(t.title),
+	t.title
+--INTO active_employee
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE de.to_date = '9999-01-01'
+GROUP BY t.title;
+
+
+--Additional Analysis: CURRENT EMPLOYEE ONLY
+--retire unique title -only current employee
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_list_title_current
+FROM employees as e
+	INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND t.to_date = '9999-01-01'
+ORDER BY e.emp_no;
+
+SELECT 
+	COUNT(emp_no)
+FROM retirement_List_title_current;
+
+SELECT 
+	COUNT(emp_no)
+FROM retirement_List_title;
+
+
+
+SELECT DISTINCT ON (rlt.emp_no) rlt.emp_no,
+rlt.first_name,
+rlt.last_name,
+rlt.title
+INTO unique_titles_current
+FROM retirement_list_title_current as rlt
+ORDER BY rlt.emp_no, rlt.to_date DESC;
+
+SELECT 
+	COUNT (emp_no)
+FROM unique_titles_current;
+
+
+SELECT 
+	COUNT (emp_no)
+FROM unique_titles;
+
+--Count by Titles
+SELECT 
+	COUNT(title),
+	title
+INTO retiring_titles_current
+FROM unique_titles_current
+GROUP BY (title)
+ORDER BY COUNT(title)DESC;
+
+SELECT * FROM retiring_titles_current;
